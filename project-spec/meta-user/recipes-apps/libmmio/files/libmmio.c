2691701c2ea89e80ab193fc3782271d5383debc2
Oleg V <oleg@localhost.localdomain>
2019-03-17 14:14:51 +0300

    Helper function for memory-mapped data read skipping zero data

--- project-spec/meta-user/recipes-apps/libmmio/files/libmmio.c
+++ project-spec/meta-user/recipes-apps/libmmio/files/libmmio.c
@@ -53,3 +53,38 @@ int mm_write16(uint32_t volatile* addr, uint16_t const* buff, unsigned word_cnt)
 	}
 	return 0;
 }
+
+/*
+ * Try read data while optionally skipping leading zero data words. The routine reads 32 bit words from the given
+ * address and then put them as 16 bit words to the buffer. If skipz is not zero it defines the number of
+ * zero 32 bit words to skip before giving up and returning 0. Note that since the routine reads data by
+ * 2 words at once it may read 1 more word than requested so the caller should provide the additional space
+ * in the buffer. The routine returns the number of 16 bit data words read or -1 in case the address is
+ * not properly aligned.
+ */
+int mm_read16_ex(uint32_t volatile const* addr, uint16_t* buff, unsigned word_cnt, unsigned skipz)
+{
+	uint16_t *ptr, *end;
+	if (!IS_ALIGNED(addr))
+		return -1;
+	ptr = buff;
+	end = buff + word_cnt;
+	while (ptr < end)
+	{
+		uint16_t w;
+		uint32_t v = *addr;
+		if (skipz && !v) {
+			if (!--skipz)
+				return 0;
+			continue;
+		}
+		w = v;
+		if (!skipz || w) {
+			*ptr++ = w;
+		}
+		w = v >> 16;
+		*ptr++ = w;
+		skipz = 0;
+	}
+	return ptr - buff;
+}
--- project-spec/meta-user/recipes-apps/libmmio/files/mmio.h
+++ project-spec/meta-user/recipes-apps/libmmio/files/mmio.h
@@ -22,3 +22,12 @@ int mm_write(uint32_t volatile* addr, uint32_t const* buff, unsigned word_cnt);
 
 int mm_write16(uint32_t volatile* addr, uint16_t const* buff, unsigned word_cnt);
 
+/*
+ * Try read data while optionally skipping leading zero data words. The routine reads 32 bit words from the given
+ * address and then put them as 16 bit words to the buffer. If skipz is not zero it defines the number of
+ * zero 32 bit words to skip before giving up and returning 0. Note that since the routine reads data by
+ * 2 words at once it may read 1 more word than requested so the caller should provide the additional space
+ * in the buffer. The routine returns the number of 16 bit data words read or -1 in case the address is
+ * not properly aligned.
+ */
+int mm_read16_ex(uint32_t volatile const* addr, uint16_t* buff, unsigned word_cnt, unsigned skipz);
